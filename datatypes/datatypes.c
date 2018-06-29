2e739a43c0cb42340578573c169a3ebf243ea8aa
pratik2799 <pithore.pratik@gmail.com>
2018-06-29 23:10:43 +0530

    added datatypes

---
+++ datatypes/datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+char b;
+float num;
+b = 'p';
+a = 25;
+num = 3.14;
+printf("a = %d \n",a);
+printf("b = %c\n",b);
+printf("num = %f\n",num);
+return 0;
+}
